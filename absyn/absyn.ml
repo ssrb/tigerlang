d7ffb34c0fd373170057ec492a68137e92ccb6b7
ssrb <sb@unhexhexium.net>
2016-11-10 10:54:19 +1300

    travis env test

--- absyn/absyn.ml
+++ absyn/absyn.ml
@@ -21,7 +21,7 @@ and exp =
     | NilExp
     | IntExp of int
     | StringExp of string * pos
-    | CallExp of { func: symbol; args: exp list; pos: pos }
+    | CallExp of callexp
     | OpExp of { left: exp; oper: oper; right: exp; pos: pos }
     | RecordExp of {fields: (symbol * exp * pos) list; typ: symbol; pos: pos}
     | SeqExp of (exp * pos) list
@@ -32,6 +32,7 @@ and exp =
     | BreakExp of pos
     | LetExp of { decs: dec list; body: exp; pos: pos }
     | ArrayExp of { typ: symbol; size: exp; init: exp; pos: pos }
+and callexp = { func: symbol; args: exp list; pos: pos }
 and dec = 
     | FunctionDec of fundec list
     | VarDec of { name: symbol; escape: bool ref; typ: (symbol * pos) option; init: exp; pos: pos }
