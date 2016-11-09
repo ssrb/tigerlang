b2f6ea552d51262b1ead2ffbf9782ec1a85f093b
ssrb <sb@unhexhexium.net>
2016-11-10 11:19:56 +1300

    Attempt at fixing the build

--- absyn/absyn.ml
+++ absyn/absyn.ml
@@ -22,17 +22,25 @@ and exp =
     | IntExp of int
     | StringExp of string * pos
     | CallExp of callexp
-    | OpExp of { left: exp; oper: oper; right: exp; pos: pos }
-    | RecordExp of {fields: (symbol * exp * pos) list; typ: symbol; pos: pos}
+    | OpExp of opexp
+    | RecordExp of recordexp
     | SeqExp of (exp * pos) list
-    | AssignExp of { var: var; exp: exp; pos: pos }
-    | IfExp of { test: exp; then': exp; else': exp option; pos: pos }
-    | WhileExp of { test: exp; body: exp; pos: pos }
-    | ForExp of { var: symbol; escape: bool ref; lo: exp; hi: exp; body: exp; pos: pos }
+    | AssignExp of assignexp
+    | IfExp of ifexp
+    | WhileExp of whileexp
+    | ForExp of forexp
     | BreakExp of pos
-    | LetExp of { decs: dec list; body: exp; pos: pos }
-    | ArrayExp of { typ: symbol; size: exp; init: exp; pos: pos }
+    | LetExp of letexp
+    | ArrayExp of arrayexp
 and callexp = { func: symbol; args: exp list; pos: pos }
+and opexp = { left: exp; oper: oper; right: exp; pos: pos }
+and recordexp = {fields: (symbol * exp * pos) list; typ: symbol; pos: pos}
+and assignexp = { var: var; exp: exp; pos: pos }
+and ifexp = { test: exp; then': exp; else': exp option; pos: pos }
+and whileexp = { test: exp; body: exp; pos: pos }
+and forexp = { var: symbol; escape: bool ref; lo: exp; hi: exp; body: exp; pos: pos }
+and letexp = { decs: dec list; body: exp; pos: pos }
+and arrayexp = { typ: symbol; size: exp; init: exp; pos: pos }
 and dec = 
     | FunctionDec of fundec list
     | VarDec of { name: symbol; escape: bool ref; typ: (symbol * pos) option; init: exp; pos: pos }
