0f72372fb12481b279ecb2e62ac8aeb884241b24
ssrb <sb@unhexhexium.net>
2016-11-14 15:13:48 +1300

    Print AST

--- absyn/prabsyn.ml
+++ absyn/prabsyn.ml
@@ -33,49 +33,48 @@ let print (outstream, e0) =
 
   let rec var (v,d) = 
   	match v with
-  	| A.SimpleVar(s,p) -> (indent d; say "SimpleVar("; say(Symbol.name s); say ")")
-    | A.FieldVar(v,s,p) -> (indent d; sayln "FieldVar("; var(v,d+1); sayln ","; indent(d+1); say(Symbol.name s); say ")")
-    | A.SubscriptVar(v,e,p) -> (indent d; sayln "SubscriptVar("; var(v,d+1); sayln ","; exp(e,d+1); say ")")
+  	| A.SimpleVar (s,p) -> (indent d; say "SimpleVar("; say(Symbol.name s); say ")")
+    | A.FieldVar (v,s,p) -> (indent d; sayln "FieldVar("; var(v,d+1); sayln ","; indent(d+1); say(Symbol.name s); say ")")
+    | A.SubscriptVar (v,e,p) -> (indent d; sayln "SubscriptVar("; var(v,d+1); sayln ","; exp(e,d+1); say ")")
    
   and exp (e, d) =
 	match e with
 	| A.VarExp v -> (indent d; sayln "VarExp("; var(v,d+1); say ")")
     | A.NilExp -> (indent d; say "NilExp")
     | A.IntExp i -> (indent d; say "IntExp("; say(Int.toString i); say ")")
-    | A.StringExp(s,p) -> (indent d; say "StringExp(\""; say s; say "\")")
-    | A.CallExp({func,args,pos}) -> (indent d; say "CallExp("; say(Symbol.name func); say ",["; dolist d exp args; say "])")
-    | A.OpExp{left,oper,right,pos} -> (indent d; say "OpExp("; say(opname oper); sayln ","; exp(left,d+1); sayln ","; exp(right,d+1); say ")")
-    | A.RecordExp{fields,typ,pos} ->
+    | A.StringExp (s,p) -> (indent d; say "StringExp(\""; say s; say "\")")
+    | A.CallExp {func;args;pos} -> (indent d; say "CallExp("; say(Symbol.name func); say ",["; dolist d exp args; say "])")
+    | A.OpExp {left;oper;right;pos} -> (indent d; say "OpExp("; say(opname oper); sayln ","; exp(left,d+1); sayln ","; exp(right,d+1); say ")")
+    | A.RecordExp {fields;typ;pos} ->
 	    let f((name,e,pos),d) = 
 			(indent d; say "("; say(Symbol.name name);
 			 sayln ","; exp(e,d+1);
 			 say ")")
-	     in (indent d; say "RecordExp("; say(Symbol.name typ); 
-	        sayln ",["; dolist d f fields; say "])" )
-    | A.SeqExp l -> (indent d; say "SeqExp["; dolist d exp (map #1 l); say "]")
-    | A.AssignExp{var=v,exp=e,pos} -> (indent d; sayln "AssignExp("; var(v,d+1); sayln ","; exp(e,d+1); say ")")
-    | A.IfExp{test,then',else',pos} -> (indent d; sayln "IfExp("; exp(test,d+1); sayln ",";  exp(then',d+1);
+	     in (indent d; say "RecordExp("; say(Symbol.name typ); sayln ",["; dolist d f fields; say "])" )
+    | A.SeqExp l -> (indent d; say "SeqExp["; (*dolist d exp (map #1 l);*) say "]")
+    | A.AssignExp {var=v;exp=e;pos} -> (indent d; sayln "AssignExp("; var(v,d+1); sayln ","; exp(e,d+1); say ")")
+    | A.IfExp {test;then';else';pos} -> (indent d; sayln "IfExp("; exp(test,d+1); sayln ",";  exp(then',d+1);
      match else' with
 	 | None -> ()
 	 | Some e -> (sayln ","; exp(e,d+1)); say ")")
-  	| A.WhileExp{test,body,pos} -> (indent d; sayln "WhileExp("; exp(test,d+1); sayln ","; exp(body,d+1); say ")")
-    | A.ForExp{var=v,escape=b,lo,hi,body,pos} ->
+  	| A.WhileExp {test;body;pos} -> (indent d; sayln "WhileExp("; exp(test,d+1); sayln ","; exp(body,d+1); say ")")
+    | A.ForExp {var=v;escape=b;lo;hi;body;pos} ->
 		(indent d; sayln "ForExp(";
 		 say(Symbol.name v); say ","; say(Bool.toString (!b)); sayln ",";
 		 exp(lo,d+1); sayln ","; exp(hi,d+1); sayln ",";
 		 exp(body,d+1); say ")")
     | A.BreakExp p -> (indent d; say "BreakExp")
-    | A.LetExp{decs,body,pos} -> (indent d; say "LetExp(["; dolist d dec decs; sayln "],"; exp(body,d+1); say")")
-    | A.ArrayExp{typ,size,init,pos} -> (indent d; say "ArrayExp("; say(Symbol.name typ); sayln ","; exp(size,d+1); sayln ","; exp(init,d+1); say ")")
+    | A.LetExp {decs;body;pos} -> (indent d; say "LetExp(["; dolist d dec decs; sayln "],"; exp(body,d+1); say")")
+    | A.ArrayExp {typ;size;init;pos} -> (indent d; say "ArrayExp("; say(Symbol.name typ); sayln ","; exp(size,d+1); sayln ","; exp(init,d+1); say ")")
 
   and dec(e, d) = 
 	match e with    
 	| A.FunctionDec l ->
-	    (let field({name,escape,typ,pos},d) = 
+	    (let field({name;escape;typ;pos},d) = 
 			(indent d; say "("; say(Symbol.name name);
 			 say ","; say(Bool.toString(!escape)); 
 			 say ","; say(Symbol.name typ); say ")")
-		and f({name,params,result,body,pos},d) =
+		and f({name;params;result;body;pos},d) =
 		   (indent d; say "("; say (Symbol.name name); say ",[";
 		    dolist d field params; sayln "],";
 		    (match result with 
@@ -83,14 +82,14 @@ let print (outstream, e0) =
 			 | Some(s,_) -> (say "SOME("; say(Symbol.name s); say ")"));
 		    sayln ","; exp(body,d+1); say ")")
 	     in indent d; say "FunctionDec["; dolist d f l; say "]")
-    | A.VarDec{name,escape,typ,init,pos} ->
+    | A.VarDec{name;escape;typ;init;pos} ->
 	   (indent d; say "VarDec("; say(Symbol.name name); say ",";
 	    say(Bool.toString (!escape)); say ",";
-	    case typ of NONE => say "NONE" 
-		      | SOME(s,p)=> (say "SOME("; say(Symbol.name s); say ")");
-            sayln ","; exp(init,d+1); say ")")
+	    (match typ with 
+    	| None -> say "NONE" 
+		| Some(s,p) -> (say "SOME("; say(Symbol.name s); say ")"); sayln ","; exp(init,d+1); say ")"))
     | A.TypeDec l ->
-	 (let fun tdec({name,ty=t,pos},d) = (indent d; say"("; 
+	 (let tdec({name;ty=t;pos},d) = (indent d; say"("; 
 				  	    say(Symbol.name name); sayln ",";
 					    ty(t,d+1); say ")")
 	  in indent d; say "TypeDec["; dolist d tdec l; say "]")
@@ -99,7 +98,7 @@ let print (outstream, e0) =
   	match t with
 	| A.NameTy(s,p) -> (indent d; say "NameTy("; say(Symbol.name s); say ")")
     | A.RecordTy l ->  
-		(let fun f({name,escape,typ,pos},d) =
+		(let f({name;escape;typ;pos},d) =
 			(indent d; say "("; say (Symbol.name name);
 		         say ","; say (Bool.toString (!escape)); say ",";
 			 say (Symbol.name typ); say ")")
