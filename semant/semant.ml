0cb79a40237f791bbdb818c144d2036586ee3e71
ssrb <sb@unhexhexium.net>
2016-12-04 13:07:55 +1300

    Semant continued

--- semant/semant.ml
+++ semant/semant.ml
@@ -9,12 +9,12 @@ type expty = {exp: Translate.exp; ty: Types.ty}
 
 let rec transExp (venv, tenv, exp) = 
   match exp with
-  | A.VarExp v -> { exp = (); ty = Types.NIL }
+  | A.VarExp v -> transVar (venv, tenv, v)
   | A.NilExp -> { exp = (); ty = Types.NIL }
   | A.IntExp i -> { exp = (); ty = Types.INT }
   | A.StringExp (s,p) -> { exp = (); ty = Types.STRING }
-  | A.CallExp {func;args;pos} -> { exp = (); ty = Types.NIL }
-  | A.OpExp {left;oper;right;pos} ->
+  | A.CallExp {func; args; pos} -> { exp = (); ty = Types.NIL }
+  | A.OpExp {left; oper; right; pos} ->
     let {exp = _; ty = tyleft} = transExp (venv, tenv, left)
     and {exp = _; ty = tyright} = transExp (venv, tenv, right)
     in
@@ -22,24 +22,23 @@ let rec transExp (venv, tenv, exp) =
         { exp = (); ty = Types.INT }
     else
         (*error "integer required"*)  { exp = (); ty = Types.NIL }
-  | A.RecordExp {fields;typ;pos} -> { exp = (); ty = Types.NIL } 
+  | A.RecordExp {fields; typ; pos} -> { exp = (); ty = Types.NIL } 
   | A.SeqExp l -> { exp = (); ty = Types.NIL }
-  | A.AssignExp {var=v;exp=e;pos} -> { exp = (); ty = Types.NIL }
-  | A.IfExp {test;then';else';pos} ->
+  | A.AssignExp {var = v; exp = e; pos} -> { exp = (); ty = Types.NIL }
+  | A.IfExp {test; then'; else'; pos} ->
       (match else' with
       | None -> { exp = (); ty = Types.NIL }
       | Some e -> { exp = (); ty = Types.NIL })
-  | A.WhileExp {test;body;pos} -> { exp = (); ty = Types.NIL }
-  | A.ForExp {var=v;escape=b;lo;hi;body;pos} -> { exp = (); ty = Types.NIL }
+  | A.WhileExp {test; body; pos} -> { exp = (); ty = Types.NIL }
+  | A.ForExp {var = v; escape = b; lo; hi; body; pos} -> { exp = (); ty = Types.NIL }
   | A.BreakExp p -> { exp = (); ty = Types.NIL }
-  | A.LetExp {decs;body;pos} -> { exp = (); ty = Types.NIL }
-  | A.ArrayExp {typ;size;init;pos} -> { exp = (); ty = Types.NIL }
+  | A.LetExp {decs; body; pos} -> { exp = (); ty = Types.NIL }
+  | A.ArrayExp {typ; size; init; pos} -> { exp = (); ty = Types.NIL }
+and transVar (venv, tenv, var) = { exp = (); ty = Types.NIL }
 
 let transProg e0 = transExp (Env.base_venv, Env.base_tenv, e0)
 
-(* 
-val transVar: venv * tenv * Absyn.var -> expty
-val transExp: venv * tenv * Absyn.exp -> expty
+(*val transExp: venv * tenv * Absyn.exp -> expty
 val transDec: venv * tenv * Absyn.dec -> { venv: venv, tenv: tenv}
 val transTy: tenv * Absyn.ty -> Types.ty
 
