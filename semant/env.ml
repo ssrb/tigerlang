c47387b50fb64e74752b17260f840bd310bfe519
ssrb <sb@unhexhexium.net>
2018-01-03 15:07:04 +1300

    Populate the base type environment with built-in types: int, string, unit, nil

--- semant/env.ml
+++ semant/env.ml
@@ -4,5 +4,11 @@ type varentry = {ty : Types.ty}
 type funentry = {formals : Types.ty list; result: Types.ty}
 type enventry = Varentry of varentry | FunEntry of funentry
 
-let base_tenv = Symbol.empty
+let base_tenv = 
+    let tenv = Symbol.enter (Symbol.empty, Symbol.symbol "int", Types.INT) in
+    let tenv = Symbol.enter (tenv, Symbol.symbol "string", Types.STRING) in
+    let tenv = Symbol.enter (tenv, Symbol.symbol "nil", Types.NIL) in
+    let tenv = Symbol.enter (tenv, Symbol.symbol "unit", Types.UNIT) in
+    tenv
+
 let base_venv = Symbol.empty
