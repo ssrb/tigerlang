c3120dd9fe08844016d3cd2e0e41e063f248b85c
ssrb <sb@unhexhexium.net>
2018-01-06 17:16:32 +1300

    Internediate representation translation WIP

---
+++ irtrans/translate.mli
@@ -0,0 +1,7 @@
+type level
+type access
+
+val outermost: level
+val newLevel: {parent: level; name: Temp.label; formals: bool list} -> level
+val formals: level -> access list
+val allocLocal: level -> bool -> access
