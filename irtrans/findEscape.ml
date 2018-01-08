11739d02c92269143069dee44fe7eec73736be14
ssrb <sb@unhexhexium.net>
2018-01-08 17:02:09 +1300

    Start implementing find escape

---
+++ irtrans/findEscape.ml
@@ -0,0 +1,7 @@
+type depth = int
+type escEnv = (depth * bool ref) Symbol.table
+fun traverseVar(env: escEnv, d: depth, s: Absyn.var): unit
+fun traverseExp(env: escEnv, d: depth, s: Absyn.exp): unit
+fun traverseDecs(env: escEnv, d: depth, s: Absyn.dec list): escEnv
+
+fun findEscape(prog: Abyn.exp) : unit
