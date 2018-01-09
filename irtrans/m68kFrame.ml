fc2180ea176de06510fb4ede12cc696aba2c28c7
ssrb <sb@unhexhexium.net>
2018-01-09 16:52:41 +1300

    Push everything on the stack for now, keep track of the stack offset

--- irtrans/m68kFrame.ml
+++ irtrans/m68kFrame.ml
@@ -3,12 +3,17 @@ open Core
 module Temp = M68kTemp
 
 type access = InFrame of int | InReg of Temp.temp
-type frame = {name: Temp.label; formals: access list}
+type frame = {name: Temp.label; formals: access list; offset: int ref}
 
-let newFrame ~name ~formals = {name; formals = formals |> List.map ~f:(fun f -> InFrame 0)}
+let (++) r inc = let x = !r in r := x + inc; x
+
+let newFrame ~name ~formals = 
+let off = ref 0 in
+let formals = formals |> List.map ~f:(fun f -> InFrame (off ++ 4)) in
+{name; formals; offset = off}
 
 let name {name; _} = name
 
 let formals {formals; _} = formals
 
-let allocLocal  (f: frame)  (escape: bool) = InFrame 0
+let allocLocal f  escape = InFrame (f.offset ++ 4)
