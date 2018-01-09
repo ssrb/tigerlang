672a3f6fd3807f572953e0aa1a57a3ecc617a1aa
ssrb <sb@unhexhexium.net>
2018-01-09 15:07:09 +1300

    Stack frame coontinued

--- irtrans/m68kFrame.ml
+++ irtrans/m68kFrame.ml
@@ -1,8 +1,16 @@
+open Core
+
 module Temp = M68kTemp
-type frame = unit
+
 type access = InFrame of int | InReg of Temp.temp
+type frame = {name: Temp.label; faccess: access list}
 type nfparams = {name: Temp.label; formals: bool list} 
-let newFrame {name; formals} = ()
+
+let newFrame {name; formals} = {name; faccess = formals |> List.map ~f:(fun f -> InFrame 0)}
+
 let name frame = Temp.newlabel ()
-let formals f = []
+
+let formals {faccess; _} = faccess
+
 let allocLocal  (f: frame)  (escape: bool) = InFrame 0
+
