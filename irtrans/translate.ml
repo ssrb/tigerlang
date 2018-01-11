fdd8ea3443473f52fbaf3ce5988b05d4a942ff97
ssrb <sb@unhexhexium.net>
2018-01-11 18:53:16 +1300

    Shouldn't be public

--- irtrans/translate.ml
+++ irtrans/translate.ml
@@ -10,10 +10,6 @@ val outermost: level
 val newLevel: parent:level -> name:Temp.label -> formals:bool list -> level
 val formals: level -> access list
 val allocLocal: level -> bool -> access
-
-val unEx: exp -> Tree.exp
-val unNx: exp -> Tree.stm
-(*val unCx: exp -> Temp.label * Temp.label -> Tree.stm*)
 end
 
 module F = functor(Frame : Frame.T) ->
