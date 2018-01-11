6ee379604fe2b6b772dd5b5da1452d8fc3b84a2f
ssrb <sb@unhexhexium.net>
2018-01-11 15:08:21 +1300

    Make it easy to check for level equality

--- irtrans/translate.ml
+++ irtrans/translate.ml
@@ -20,22 +20,23 @@ open Frame
 open Temp
 
 type exp = Ex of Tree.exp | Nx of Tree.stm | Cx of (Tree.label * Tree.label -> Tree.stm)
-type level = Outermost | Level of {level: level; frame: Frame.frame}
+type _level = {level: level; frame: Frame.frame}
+and level = Outermost | Level of _level * unit ref
 type access = level * Frame.access
 
 let outermost = Outermost
 
 let newLevel ~parent ~name ~formals =
-Level {level = parent; frame = Frame.newFrame ~name ~formals:(true::formals)}
+Level ({level = parent; frame = Frame.newFrame ~name ~formals:(true::formals)}, ref ())
 
 let formals lvl = 
 match lvl with
 | Outermost -> assert(false)
-| Level {frame; _} -> frame |> Frame.formals |> List.tl_exn |> List.map ~f:(fun acc -> (lvl, acc))
+| Level ({frame; _}, _) -> frame |> Frame.formals |> List.tl_exn |> List.map ~f:(fun acc -> (lvl, acc))
 
 let allocLocal lvl escape = 
 match lvl with
 | Outermost -> assert(false)
-| Level {frame; _} -> (lvl, Frame.allocLocal frame escape)
+| Level ({frame; _}, _) -> (lvl, Frame.allocLocal frame escape)
 
 end
