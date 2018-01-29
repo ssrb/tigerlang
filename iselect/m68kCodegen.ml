de876cd21e5930c572c2c924237557eb91d03a8e
ssrb <sb@unhexhexium.net>
2018-01-30 01:26:04 +1300

    Maximal munch

--- iselect/m68kCodegen.ml
+++ iselect/m68kCodegen.ml
@@ -24,14 +24,23 @@ let codegen frame stm =
             match e1 with
             | T.MEM (T.BINOP (T.PLUS, e1, (T.CONST j)))
             | T.MEM (T.BINOP (T.PLUS, (T.CONST j), e1)) ->
-                emit(A.OPER {assem = "move.l " ^ Int.to_string (j / 4) ^ "(s1)," ^ Int.to_string (i / 4) ^ "(s0)"; dst = []; src = [munchAddrExp e0; munchAddrExp e1]; jump = None})
+                if i = j then
+                    emit(A.MOVE {assem = "move.l " ^ Int.to_string (j / 4) ^ "(s1)," ^ Int.to_string (i / 4) ^ "(s0)"; dst = munchAddrExp e0; src = munchAddrExp e1})
+                else
+                    emit(A.OPER {assem = "move.l " ^ Int.to_string (j / 4) ^ "(s1)," ^ Int.to_string (i / 4) ^ "(s0)"; dst = []; src = [munchAddrExp e0; munchAddrExp e1]; jump = None})
   
             | T.MEM (T.BINOP (T.MINUS, e1, (T.CONST j)))
             | T.MEM (T.BINOP (T.MINUS, (T.CONST j), e1)) ->
                 emit(A.OPER {assem = "move.l " ^ Int.to_string (-j / 4) ^ "(s1)," ^ Int.to_string (i / 4) ^ "(s0)"; dst = []; src = [munchAddrExp e0; munchAddrExp e1]; jump = None})
 
+            | T.MEM (T.CONST i) ->
+                emit(A.OPER {assem = "move.l $" ^ Int.to_string (i / 4) ^ ",(s0)"; dst = []; src = [munchAddrExp e0]; jump = None})
+
             | T.MEM e1 ->
                 emit(A.OPER {assem = "move.l (s1)," ^ Int.to_string (i / 4) ^ "(s0)"; dst = []; src = [munchAddrExp e0; munchAddrExp e1]; jump = None})
+
+            | T.CONST i ->
+                emit(A.OPER {assem = "move.l #$" ^ Int.to_string (i / 4) ^ ",(s0)"; dst = []; src = [munchAddrExp e0]; jump = None})
 
             | e1 ->
                 emit(A.OPER {assem = "move.l s1," ^ Int.to_string (i / 4) ^ "(s0)"; dst = []; src = [munchAddrExp e0; munchDataExp e1]; jump = None})
@@ -47,10 +56,19 @@ let codegen frame stm =
   
             | T.MEM (T.BINOP (T.MINUS, e1, (T.CONST j)))
             | T.MEM (T.BINOP (T.MINUS, (T.CONST j), e1)) ->
-                emit(A.OPER {assem = "move.l " ^ Int.to_string (-j / 4) ^ "(s1)," ^ Int.to_string (-i / 4) ^ "(s0)"; dst = []; src = [munchAddrExp e0; munchAddrExp e1]; jump = None})
+                if i = j then
+                    emit(A.MOVE {assem = "move.l " ^ Int.to_string (-j / 4) ^ "(s1)," ^ Int.to_string (-i / 4) ^ "(s0)"; dst = munchAddrExp e0; src = munchAddrExp e1})
+                else
+                    emit(A.OPER {assem = "move.l " ^ Int.to_string (-j / 4) ^ "(s1)," ^ Int.to_string (-i / 4) ^ "(s0)"; dst = []; src = [munchAddrExp e0; munchAddrExp e1]; jump = None})
+
+            | T.MEM (T.CONST i) ->
+                emit(A.OPER {assem = "move.l $" ^ Int.to_string (i / 4) ^ ",(s0)"; dst = []; src = [munchAddrExp e0]; jump = None})
 
             | T.MEM e1 ->
                 emit(A.OPER {assem = "move.l (s1)," ^ Int.to_string (-i / 4) ^ "(s0)"; dst = []; src = [munchAddrExp e0; munchAddrExp e1]; jump = None})
+
+            | T.CONST i ->
+                emit(A.OPER {assem = "move.l #$" ^ Int.to_string (i / 4) ^ ",(s0)"; dst = []; src = [munchAddrExp e0]; jump = None})
 
             | e1 ->
                 emit(A.OPER {assem = "move.l s1," ^ Int.to_string (-i / 4) ^ "(s0)"; dst = []; src = [munchAddrExp e0; munchDataExp e1]; jump = None})
@@ -67,18 +85,39 @@ let codegen frame stm =
             | T.MEM (T.BINOP (T.MINUS, (T.CONST j), e1)) ->
                 emit(A.OPER {assem = "move.l " ^ Int.to_string (-j / 4) ^ "(s1),(s0)"; dst = []; src = [munchAddrExp e0; munchAddrExp e1]; jump = None})
 
+            | T.MEM (T.CONST i) ->
+                emit(A.OPER {assem = "move.l $" ^ Int.to_string (i / 4) ^ ",(s0)"; dst = []; src = [munchAddrExp e0]; jump = None})
+
             | T.MEM e1 ->
-                emit(A.OPER {assem = "move.l (s1),(s0)"; dst = []; src = [munchAddrExp e0; munchAddrExp e1]; jump = None})
+                if e0 <> e1 then
+                    emit(A.OPER {assem = "move.l (s1),(s0)"; dst = []; src = [munchAddrExp e0; munchAddrExp e1]; jump = None});
+
+            | T.CONST i ->
+                emit(A.OPER {assem = "move.l #$" ^ Int.to_string (i / 4) ^ ",(s0)"; dst = []; src = [munchAddrExp e0]; jump = None})
 
             | e1 ->
                 emit(A.OPER {assem = "move.l s1,(s0)"; dst = []; src = [munchAddrExp e0; munchDataExp e1]; jump = None})
         end 
 
-        | T.MOVE(e0, e1) -> emit(A.OPER {assem = "move.l s1,s0"; dst = []; src = [munchDataExp e0; munchDataExp e1]; jump = None})
+        | T.MOVE(e0, e1) -> 
+            emit(A.OPER {assem = "move.l s1,s0"; dst = []; src = [munchDataExp e0; munchDataExp e1]; jump = None})
+
+        | T.LABEL label -> 
+            emit(A.LABEL {assem = (Symbol.name label) ^ ":"; lab = label})
+        
+        | JUMP (e0, lbls) ->
+            emit(A.OPER {assem = ""; dst = []; src = []; jump = None})
+        (*| CJUMP (relop, e0, e1, t, f*)
+        (*| EXP of exp*)
+
         | _ -> assert(false)
 
     and munchDataExp = function
-        | T.MEM(T.BINOP(T.PLUS, T.CONST i, e1)) -> result(fun r -> emit(A.OPER {assem = ""; dst = [r]; src = [munchAddrExp e1]; jump = None}))
+        | T.MEM(T.BINOP(T.PLUS, T.CONST i, e0)) -> result(fun r -> emit(A.OPER {assem = "move.l " ^ Int.to_string (i / 4) ^ "(s0),d0"; dst = [r]; src = [munchAddrExp e0]; jump = None}))
+        | T.MEM(T.BINOP(T.MINUS, T.CONST i, e0)) -> result(fun r -> emit(A.OPER {assem = "move.l " ^ Int.to_string (-i / 4) ^ "(s0),d0"; dst = [r]; src = [munchAddrExp e0]; jump = None}))
+        | T.MEM(T.CONST i) -> result(fun r -> emit(A.OPER {assem = "move.l $"^ Int.to_string i ^ ",d0"; dst = [r]; src = []; jump = None}))
+        | T.MEM(e0) -> result(fun r -> emit(A.OPER {assem = "move.l (s0),d0"; dst = [r]; src = [munchAddrExp e0]; jump = None}))
+        | T.CONST i -> result(fun r -> emit(A.OPER {assem = "move.l #$" ^ Int.to_string i ^ ",d0"; dst = [r]; src = []; jump = None}))        
         | _ -> assert(false)
     and munchAddrExp = function
         | _ -> assert(false)
