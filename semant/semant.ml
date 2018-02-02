accfa9a3ec91c356e602f67a1933a51120d9d323
ssrb <sb@unhexhexium.net>
2018-02-02 15:39:16 +1300

    Semantic analysis fix-up

--- semant/semant.ml
+++ semant/semant.ml
@@ -165,7 +165,10 @@ let rec transExp (venv, tenv, lvl, exp, break) =
         if not (type_equal then'.ty else'.ty) then
           raise (Semantic_error "If-then-else type is inconsistent");
         {exp = T.transIf (test.exp, then'.exp, Some else'.exp); ty = then'.ty}
-      | None -> {exp = T.transIf (test.exp, then'.exp, None); ty = Types.UNIT}
+      | None -> 
+        if not (type_equal then'.ty Types.UNIT) then
+          raise (Semantic_error "If-then statement must have unit type");
+        {exp = T.transIf (test.exp, then'.exp, None); ty = Types.UNIT}
     end
 
   | WhileExp w ->
