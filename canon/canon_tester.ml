789a74574db7e4a253f982919c51d4c437a28d66
ssrb <sb@unhexhexium.net>
2018-02-03 22:05:20 +1300

    Add some newline to the pretty printers

--- canon/canon_tester.ml
+++ canon/canon_tester.ml
@@ -6,7 +6,7 @@ module Canon = Canon.F(Tree)
 
 open Core
 
-let () = 
+let _ = 
 
 let fragments = 
 	let lexbuf = Lexing.from_channel In_channel.stdin in
@@ -17,8 +17,14 @@ let f frag =
 	match frag with
 	| Translate.Frame.PROC proc -> 
 		let ts = proc.body |> Canon.linearize |> Canon.basicBlocks |> Canon.traceSchedule in
-		List.iter ts ~f:(fun t -> Tree.sexp_of_stm t |> Sexp.output_hum Out_channel.stdout )
-	| Translate.Frame.STRING (lab, str) -> (Tree.LABEL lab) |> Tree.sexp_of_stm |> Sexp.output_hum Out_channel.stdout
+		List.iter ts ~f:(fun t -> 
+			Tree.sexp_of_stm t |> Sexp.output_hum Out_channel.stdout;
+			Out_channel.newline Out_channel.stdout)
+	| Translate.Frame.STRING (lab, str) -> 
+		(Tree.LABEL lab) |> Tree.sexp_of_stm |> Sexp.output_hum Out_channel.stdout;
+		Out_channel.newline Out_channel.stdout;
+		Out_channel.print_endline str
 in
 
-fragments |> List.iter ~f:f
+let _ = fragments |> List.iter ~f:f
+in Out_channel.flush Out_channel.stdout
