82d12666c8909c7ccd40ab8d0a0036beaf363054
ssrb <sb@unhexhexium.net>
2018-02-08 16:48:20 +1300

    Register allocation

--- regalloc/graph.mli
+++ regalloc/graph.mli
@@ -1,23 +1,19 @@
-signature GRAPH =
-sig
-    type graph
-    type node
-    
-    val nodes: graph -> node list
-    val succ: node -> node list
-    val pred: node -> node list
-    val adj: node -> node list   (* succ+pred *)
-    val eq: node*node -> bool
+type graph
+type node
 
-    val newGraph: unit -> graph
-    val newNode : graph -> node
-    exception GraphEdge
-    val mk_edge: {from: node, to: node} -> unit
-    val rm_edge: {from: node, to: node} -> unit
+val nodes: graph -> node list
+val succ: node -> node list
+val pred: node -> node list
+val adj: node -> node list   (* succ+pred *)
+val eq: node*node -> bool
 
-    structure Table : TABLE 
-    sharing type Table.key = node
+val newGraph: unit -> graph
+val newNode : graph -> node
+exception GraphEdge
+val mk_edge: {from: node, to: node} -> unit
+val rm_edge: {from: node, to: node} -> unit
 
-    val nodename: node->string  (* for debugging only *)
+type Table : Table with type key = node
+(*sharing type Table.key = node*)
 
-end
+val nodename: node->string  (* for debugging only *)
