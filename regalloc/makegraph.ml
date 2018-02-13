786e492334d377e93ac458fc5534618b8d404776
ssrb <sb@unhexhexium.net>
2018-02-13 16:51:15 +1300

    Flow graph

--- regalloc/makegraph.ml
+++ regalloc/makegraph.ml
@@ -5,12 +5,14 @@ module Flow = Flowgraph.F(Assem.Temp)
 open Flow
 open Core
 open Assem
+open Graph
 
 let instrs2graph instrs =
     let f (fgraph, nodes) instr = 
         match instr with 
         | OPER op -> 
             let node = Graph.newNode fgraph.control in
+            Graph.mk_edge {f = node; t = node};
             let fgraph = { 
                 fgraph with 
                 def = Graph.Table.enter (fgraph.def, node, op.dst);
@@ -19,7 +21,16 @@ let instrs2graph instrs =
             } in
             (fgraph, node::nodes)
 	    | LABEL lab -> (fgraph, nodes)
-	    | MOVE mv -> (fgraph, nodes)
+	    | MOVE mv ->
+            let node = Graph.newNode fgraph.control in
+            Graph.mk_edge {f = node; t = node};
+            let fgraph = { 
+                fgraph with 
+                def = Graph.Table.enter (fgraph.def, node, [ mv.dst ]);
+                use = Graph.Table.enter (fgraph.use, node, [ mv.dst ]);
+                ismove = Graph.Table.enter (fgraph.ismove, node, true)
+            } in
+            (fgraph, node::nodes)
     in List.fold ~init:({ 
         control = Graph.newGraph (); 
         def = Graph.Table.empty; 
