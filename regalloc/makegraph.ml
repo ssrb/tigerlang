4f8fcfc848a9f0fe4e3d29ad82a79541e51d4a7a
ssrb <sb@unhexhexium.net>
2018-02-13 16:43:33 +1300

    Flesh out the function building the flowgraph

--- regalloc/makegraph.ml
+++ regalloc/makegraph.ml
@@ -1,12 +1,30 @@
 module F = functor(Assem : Assem.T) -> struct
+
 module Flow = Flowgraph.F(Assem.Temp)
+
 open Flow
-let instrs2graph instrs = (
-    { 
+open Core
+open Assem
+
+let instrs2graph instrs =
+    let f (fgraph, nodes) instr = 
+        match instr with 
+        | OPER op -> 
+            let node = Graph.newNode fgraph.control in
+            let fgraph = { 
+                fgraph with 
+                def = Graph.Table.enter (fgraph.def, node, op.dst);
+                use = Graph.Table.enter (fgraph.use, node, op.dst);
+                ismove = Graph.Table.enter (fgraph.ismove, node, false)
+            } in
+            (fgraph, node::nodes)
+	    | LABEL lab -> (fgraph, nodes)
+	    | MOVE mv -> (fgraph, nodes)
+    in List.fold ~init:({ 
         control = Graph.newGraph (); 
         def = Graph.Table.empty; 
         use = Graph.Table.empty;
         ismove = Graph.Table.empty
-    }
-    , [])
+    }, []) ~f:f instrs
+
 end
