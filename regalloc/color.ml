1fc587fdb8fa28de61b0b2acb3b08c81fd21645e
ssrb <sb@unhexhexium.net>
2018-02-28 12:41:29 +1300

    Back to it

--- regalloc/color.ml
+++ regalloc/color.ml
@@ -3,5 +3,30 @@ struct
 module Temp = Frame.Temp
 type allocation = Frame.register Temp.Table.table
 type color = {interference: Liveness.igraph; initial: allocation; spillCost: Graph.node -> int; registers: Frame.register list} 
-let color color  = (Temp.Table.empty, [])
+let color color  = 
+
+    let precolored = [] in
+    let initial = [] in
+    let simplifyWorklist = [] in
+    let freezeWorklist = []in
+    let spillWorklist = [] in
+    let spilledNodes = [] in
+    let coalescedNodes = [] in
+    let coloredNodes = [] in
+    let selectStack = [] in
+
+    let coalescedMoves = [] in
+    let constrainedMoves = [] in
+    let frozenMoves = [] in
+    let worklistMoves = [] in
+    let activeMoves = [] in
+
+    let adjSet = () in
+    let adjList = () in
+    let degree = () in
+    let moveList = () in
+    let alias = () in
+    let color = () in
+    
+    (Temp.Table.empty, [])
 end
