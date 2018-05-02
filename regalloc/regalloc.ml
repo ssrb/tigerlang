0b448b109c11947b49e1fd5f90f634edc27b5c8d
ssrb <sb@unhexhexium.net>
2018-05-02 15:28:55 +1200

    Bug fix

--- regalloc/regalloc.ml
+++ regalloc/regalloc.ml
@@ -25,8 +25,8 @@ let rec alloc (asm, frame) =
         let rewriteProgram' asm (spill : Temp.temp) = 
 
             let memory = Frame.exp ((Frame.allocLocal frame true), (Tree.TEMP { temp = Frame.fp; ptr = true })) in
-            let fetch t = Codegen.codegen frame (Tree.MOVE (memory, (Tree.TEMP { temp = t; ptr = false }))) in
-            let store t = Codegen.codegen frame (Tree.MOVE ((Tree.TEMP { temp = t; ptr = false }), memory)) in
+            let fetch t = Codegen.codegen frame (Tree.MOVE ((Tree.TEMP { temp = t; ptr = false }), memory)) in
+            let store t = Codegen.codegen frame (Tree.MOVE (memory, (Tree.TEMP { temp = t; ptr = false }))) in
 
             let rewriteOperands fs ops  = 
                 if List.exists ~f:(fun (o : Assem.Variable.t) -> o.temp = spill) ops then
