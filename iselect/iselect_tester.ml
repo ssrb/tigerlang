49bbde65e1cab6ba8c328cf9915252ce668c4d19
ssrb <sb@unhexhexium.net>
2018-09-03 17:48:12 +1200

    Improve instruction selection test program

--- iselect/iselect_tester.ml
+++ iselect/iselect_tester.ml
@@ -1,20 +1,19 @@
-module Lexer = Tigerlex.F(Parsertokens)
-module Translate = Translate.F(M68kFrame)
-module Semant = Semant.F(Translate)
-module Canon = Canon.F(M68kFrame.Tree)
-(*module M68K = M68kCodegen*)
-module M68K = M68000.Generator
 open Core
 
-let _ = 
+module MAIN = functor(M68K: Codegen.T) -> struct
 
-let open Out_channel in
+module Lexer = Tigerlex.F(Parsertokens)
+module Frame = M68K.Frame
+module Translate = Translate.F(Frame)
+module Semant = Semant.F(Translate)
+module Canon = Canon.F(Frame.Tree)
+
+open Out_channel
 
 let fragments = 
 	let lexbuf = Lexing.from_channel  In_channel.stdin in
 	Tigerparse.prog Lexer.read lexbuf
 	|> Semant.transProg2
-in
 
 let f frag =
 	match frag with
@@ -26,32 +25,42 @@ let f frag =
 			|> List.map ~f:(M68K.codegen proc.frame)
 			|> List.concat
 		in
-		let asm = M68kFrame.procEntryExit2 (proc.frame, asm) in
-		let asm = M68kFrame.procEntryExit3 (proc.frame, asm) in
-		let outchannel = Out_channel.stdout in
-		M68kFrame.(
-			Out_channel.output_string outchannel asm.prolog;
+		let asm = Frame.procEntryExit2 (proc.frame, asm) in
+		let asm = Frame.procEntryExit3 (proc.frame, asm) in
+		let outchannel = stdout in
+		Frame.(
+			output_string outchannel asm.prolog;
 
 			asm.body |> List.iter ~f:(fun instr -> 
 				let asm = instr |> M68K.Assem.format_hum (fun tmp ->
-					Option.value ~default:(Temp.makestring tmp.temp) (Temp.Table.look (M68kFrame.tempMap, tmp.temp))
+					Option.value ~default:(Temp.makestring tmp.temp) (Temp.Table.look (Frame.tempMap, tmp.temp))
 				) in
 				if not (String.is_empty asm) then
 				begin 
-					Out_channel.output_string outchannel asm;
-					Out_channel.newline outchannel
+					output_string outchannel asm;
+					newline outchannel
 				end
 			);
 
-			Out_channel.output_string outchannel asm.epilog
+			output_string outchannel asm.epilog
 		)
 
 	| Translate.Frame.STRING (lbl, str) -> 
-		Out_channel.print_endline ((Symbol.name lbl) ^ ": even");
-		Out_channel.print_endline ("\tdc.l " ^ (str |> String.length |> Int.to_string));
-		Out_channel.print_endline ("\tdc.b \"" ^ (String.escaped str) ^ "\"")
+		print_endline ((Symbol.name lbl) ^ ": even");
+		print_endline ("\tdc.l " ^ (str |> String.length |> Int.to_string));
+		print_endline ("\tdc.b \"" ^ (String.escaped str) ^ "\"")
 
-in
+let test () = 
+	fragments |> List.iter ~f:f;
+	flush stdout
 
-let _ = fragments |> List.iter ~f:f in
-flush stdout
+end
+
+let _ = 
+	if (Array.length Sys.argv) > 1 then
+		let module Main = MAIN(M68000.Generator) in Main.test ()
+	else
+		let module Main = MAIN(M68kCodegen) in Main.test ()
+		
+
+	
