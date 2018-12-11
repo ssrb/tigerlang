5d765c5fe00a5f67aabea95810e3e051cce8b8be
ssrb <sb@unhexhexium.net>
2018-12-11 15:22:58 +1300

    Read from a file rather than stdin

--- main/tiger.ml
+++ main/tiger.ml
@@ -9,42 +9,17 @@ module TT = Temp.Table
 
 open Core
 
-
-(**
-  * Direct translation of the example of glpk's reference manual.
-  *
-  * @author Samuel Mimram
-  *)
-
-(*open Glpk
-
-let () =
-  let lp = make_problem Maximize
-             [|10.; 6.; 4.|]
-             [|
-               [|1.; 1.; 1.|];
-               [|10.; 4.; 5.|];
-               [|2.; 2.; 6.|]
-             |]
-             [| -.infinity, 100.; -.infinity, 600.; -.infinity, 300. |]
-             [| 0., infinity; 0., infinity; 0., infinity|] in
-    scale_problem lp;
-    use_presolver lp true;
-    simplex lp;
-    let prim = get_col_primals lp in
-      Printf.printf "Z: %g    x0: %g    x1: %g    x2: %g\n%!" (get_obj_val lp) prim.(0) prim.(1) prim.(2)*)
-
 let _ = 
 
 let open Out_channel in
 
 let fragments = 
-	let lexbuf = Lexing.from_channel  In_channel.stdin in
+	let lexbuf = Lexing.from_channel  (In_channel.create Sys.argv.(1)) in
 	Tigerparse.prog Lexer.read lexbuf
 	|> Semant.transProg2
 in
 
-let f frag =
+let f outchannel frag =
 	match frag with
 	| Translate.Frame.PROC proc -> 
 
@@ -59,8 +34,10 @@ let f frag =
 		let asm, allocation = Regalloc.alloc (M68kFrame.procEntryExit2 (proc.frame, asm), proc.frame) in
 
 		let asm = M68kFrame.procEntryExit3 (proc.frame, asm) in
-		let outchannel = Out_channel.stdout in
-		Out_channel.print_endline ((Symbol.name (M68kFrame.name proc.frame)) ^ ":");
+
+		Out_channel.output_string outchannel ((Symbol.name (M68kFrame.name proc.frame)) ^ ":");
+		Out_channel.newline outchannel;
+
 		M68kFrame.(
 			Out_channel.output_string outchannel asm.prolog;
 
@@ -81,13 +58,19 @@ let f frag =
 		)
 	
 	| Translate.Frame.STRING (lbl, str) -> 
-		Out_channel.print_endline ((Symbol.name lbl) ^ ": even");
-		Out_channel.print_endline ("\tdc.l " ^ (str |> String.length |> Int.to_string));
-		Out_channel.print_endline ("\tdc.b \"" ^ (String.escaped str) ^ "\"")
+		Out_channel.output_string outchannel ((Symbol.name lbl) ^ ": even");
+		Out_channel.newline outchannel;
+		Out_channel.output_string outchannel ("\tdc.l " ^ (str |> String.length |> Int.to_string));
+		Out_channel.newline outchannel;
+		Out_channel.output_string outchannel ("\tdc.b \"" ^ (String.escaped str) ^ "\"");
+		Out_channel.newline outchannel
 in
 
-let _ = 
-Out_channel.print_endline "_tigermain:";
-Out_channel.print_endline "\tpublic _tigermain";
-fragments |> List.iter ~f:f in
-()
+let outchannel = Out_channel.stdout in
+Out_channel.output_string outchannel "_tigermain:";
+Out_channel.newline outchannel;
+Out_channel.output_string outchannel "\tpublic _tigermain";
+Out_channel.newline outchannel;
+fragments |> List.iter ~f:(f outchannel);
+let son = Unix.fork_exec ~prog:"vc" ~argv:[ "vc"; "+kick13" ] () in
+Unix.waitpid_exn son
